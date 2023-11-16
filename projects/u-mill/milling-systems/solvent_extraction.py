0ede645fcccb0374673e82d81fb0422935a2998a
Valmor F. de Almeida <dealmeidavf@gmail.com>
2023-11-16 17:28:10 -0500

    mods

--- projects/u-mill/milling-systems/solvent_extraction.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# This file is part of the Cortix toolkit environment.
-# https://cortix.org
-"""
-Cortix Module
-This module is a model of the Solvent Extraction process
-"""
-
